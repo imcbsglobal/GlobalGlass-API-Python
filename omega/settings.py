ba2bc380adabda081145e941999bd0ed54771e85
Krishna Kumar <149694348+KRISHNAKUMARPS2002@users.noreply.github.com>
2025-06-10 15:48:44 +0530

    STATIC ROOT added

--- omega/settings.py
+++ omega/settings.py
@@ -162,7 +162,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = '/var/www/leads/staticfiles/' 
+STATIC_ROOT = '/var/www/Global-Glass-syncAPI/staticfiles/'
+#STATIC_ROOT = '/var/www/leads/staticfiles/' 
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
